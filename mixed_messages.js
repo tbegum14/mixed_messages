cea25bbd64284fed833c21bb207f880380715590
tbg14 <thasnima@hotmail.co.uk>
2023-06-30 05:00:07 +0100

    second statement added

--- mixed_messages.js
+++ mixed_messages.js
@@ -26,24 +26,32 @@ const statementOne = ()=>{
 }
  
 const statementTwo = ()=>{
-    let x = randomNum(4)
+    let x = randomNum(6)
     console.log(x)
     switch (x){
         case 0:
-            return "You are a "
+            return "banana"
             break;
         case 1:
-            return "You look like a "
+            return "loser"
             break;
         case 2:
-            return "You smell like a "
+            return "armpit"
             break;
         case 3:
-            return "You remind me of a "
+            return "princess"
             break;
+        case 4:
+            return "lemon"
+            break;
+        case 5:
+            return "poo"
+            break;    
         default:
             "error message"
             break;        
     }
 }
-console.log(statementOne())
+
+
+console.log(statementOne()+statementTwo())
